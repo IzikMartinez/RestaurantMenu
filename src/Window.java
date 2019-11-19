0ff394b45d2a589b6f063b90ec47be8890d9301c
Isaac <IsaacR454@gmail.com>
2019-11-19 15:25:12 -0600

    Added the Nutrition object and set Ingredient to extend Nutrition

---
+++ src/Window.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window extends JFrame {
+    private JFrame orderFrame = new JFrame("Order");
+    private JFrame checkoutFrame = new JFrame("Check Out");
+    JButton addItem; JButton checkout;
+    Window() {
+        super("Eddie's burgers");
+        setLayout(new FlowLayout());
+        setSize(new Dimension(700,900));
+        setLocationRelativeTo(null);
+        add(createButton("Add To Order", 150,40, OrderEvent));
+        add(createButton("Check Out", 200,40, CheckOutEvent));
+    }
+
+
+    private JButton createButton(String name, int width, int height, ActionListener event) {
+        JButton button = new JButton(name);
+        button.setPreferredSize(new Dimension(width, height));
+        button.addActionListener(event);
+        return button;
+    }
+
+    private ActionListener OrderEvent = e -> {
+        orderFrame.setSize(new Dimension(400,600));
+        orderFrame.setLocationRelativeTo(null);
+        orderFrame.setVisible(true);
+    };
+
+    private ActionListener CheckOutEvent = e -> {
+        checkoutFrame.setSize(new Dimension(400,600));
+        checkoutFrame.setLocationRelativeTo(null);
+        checkoutFrame.setVisible(true);
+    };
+
+
+}
