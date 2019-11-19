e529c6ca26960f048ea7f4d21d86cecb8b553d0a
Isaac <IsaacR454@gmail.com>
2019-11-19 15:24:52 -0600

    Began to write the GUI components. After the empty GUI is done, work will begin on writing the actual code that makes this program work.

--- src/main.java
+++ src/main.java
@@ -1,4 +1,9 @@
+import javax.swing.*;
+
 public class main {
     public static void main(String[] args) {
+        Window window = new Window();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
     }
 }
