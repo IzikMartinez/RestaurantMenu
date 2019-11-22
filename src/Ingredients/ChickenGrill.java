692659e761846f7fc92e151064bbf2d277823761
samrobinson1999 <56972816+samrobinson1999@users.noreply.github.com>
2019-11-22 15:47:30 -0600

    Add files via upload
    commit comment

---
+++ src/Ingredients/ChickenGrill.java
@@ -0,0 +1,13 @@
+package Ingredients;
+
+public class ChickenGrill extends Ingredient {
+
+    public ChickenGrill() {
+        calories = 50*3;
+        fat = 1*3;
+        carbohydrate = 0.0;
+        protein = 20*3;
+        serving_size = "3oz";
+        cholesterol = .03*3;
+    }
+}
---
+++ src/Ingredients/Mayonnaise.java
@@ -0,0 +1,9 @@
+package Ingredients;
+
+public class Mayonnaise extends Ingredient {
+
+    public Mayonnaise() {
+   super(94,.1,.1,10,"Mayonnaise")
+        serving_size = "1 tbsp";
+    }
+}
